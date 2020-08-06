6c297e569cfd62f4be6e85160a9038ec7d56ac03
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-08-06 10:51:15 -0500

    creating file layout

---
+++ guardian/src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import{ 
+    BrowserRouter as Router ,
+    Switch,Link,Route
+}
+from 'react-router-dom'
+
+export default class Nav extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
